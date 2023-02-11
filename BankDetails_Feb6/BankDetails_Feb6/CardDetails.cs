51f2c8e3d47838b1f1a473b049c539d1a0d32011
Sami Sunil Yaswanth <samisunilyaswanth@gmail.com>
2023-02-11 12:04:09 +0530

    bank details

---
+++ BankDetails_Feb6/BankDetails_Feb6/CardDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDetails_Feb6
+{
+    public class BankOne
+    {
+        protected void PersonalDetailsOne()
+        {
+            Console.WriteLine("Personal details");
+        }
+        private void SalaryDetailsOne()
+        {
+            Console.WriteLine("salary details"); 
+        }
+    }
+    class CardDetails:BankOne
+    {
+        static void Main(string[] args)
+        {
+           CardDetails card= new CardDetails();
+            card.PersonalDetailsOne();
+            //card.SalaryDetailsOne();  
+            
+        }
+    }
+}
---
+++ BankDetails_Feb6/BankDetails_Feb6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDetails_Feb6
+{
+    class Bank
+    {
+        protected class Personal : Bank 
+        { 
+            public void PersonalDetails()
+            {
+                Console.WriteLine("Personal Details for the bank");
+            }
+        }
+
+        private class Salary : Bank
+        {
+            public void SalaryDetails()
+            {
+                Console.WriteLine("Salary details for the bank"); 
+            }
+        }
+
+    }
+    class Program:Bank 
+    {
+        static void Main(string[] args)
+        {
+            Personal personal = new Personal();
+            personal.PersonalDetails();
+            
+        }
+    }
+}
